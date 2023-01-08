6c2b7cd667982e083888b49760d7639760fd056a
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-01-08 21:47:10 +0300

    Creating displayPage function for all 42 items

--- js/script.js
+++ js/script.js
@@ -18,6 +18,35 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function displayPage(list) {
+ //  let startIndex = (page * itemsPerPage) - itemsPerPage;
+ //  let endIndex = (page * itemsPerPage);
+   const listPage = document.querySelector('.student-list');
+   listPage.innerHTML = '';
+   
+   for (let i = 0; i < list.length; i++) {
+      console.log(list[i]);
+      listPage.innerHTML += 
+      `
+      <li class="student-item cf">
+      <div class="student-details">
+        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+        <h3> ${list[i].name.first} ${list[i].name.last}</h3>
+        <span class="email"> ${list[i].email} </span>
+      </div>
+      <div class="joined-details">
+        <span class="date">Joined ${list[i].registered.date}</span>
+      </div>
+      </li>
+      `
+      ;
+
+
+   }
+}
+
+displayPage(data);
+
 
 
 /*
