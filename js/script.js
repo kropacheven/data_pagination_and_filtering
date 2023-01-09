f47e65a8e4ebef85e1e71a3a0afb2e0911a49c95
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-01-09 21:13:43 +0300

    Adjusting picture thumbnails

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ function showPage(list, page) {
       `
       <li class="student-item cf">
       <div class="student-details">
-        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+        <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
         <h3> ${list[i].name.first} ${list[i].name.last}</h3>
         <span class="email"> ${list[i].email} </span>
       </div>
